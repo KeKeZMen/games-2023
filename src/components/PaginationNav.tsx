f9c7e4337b98cdfc1b14d90d4c6a535614d4870d
Kekez <makeskex@yandex.ru>
2024-03-14 11:21:27 +0300

    fixing pagination

--- src/components/PaginationNav.tsx
+++ src/components/PaginationNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import {
   Pagination,
   PaginationContent,
@@ -16,43 +16,76 @@ type PropsType = {
 
 export const PaginationNav: FC<PropsType> = ({ productsCount }) => {
   const searchParams = useSearchParams();
-  const page = searchParams.get("page");
   const pathname = usePathname();
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const handleParams = (page: number) => {
+  useEffect(() => {
     const current = new URLSearchParams(Array.from(searchParams.entries()));
-    current.set("page", String(page))
+    current.set("page", String(currentPage));
     let search = current.toString();
     const query = search ? `?${search}` : "";
 
     router.push(`${pathname}${query}`);
     router.refresh();
-  };
+  }, [currentPage]);
 
   return (
     <Pagination className="mb-5">
       <PaginationContent>
         <PaginationPrevious
-          onClick={() => handleParams(Number(page) > 0 ? Number(page) - 1 : 0)}
+          onClick={() => setCurrentPage(currentPage > 0 ? currentPage - 1 : 0)}
         />
+        <PaginationLink
+          isActive={0 == currentPage}
+          onClick={() => setCurrentPage(0)}
+        >
+          1
+        </PaginationLink>
 
-        {Array.from({ length: Math.ceil(productsCount / 10) }).map((_, i) => (
-          <PaginationLink
-            key={i}
-            isActive={i == Number(page)}
-            onClick={() => handleParams(i)}
+        <div className="max-w-[120px] overflow-x-hidden">
+          <div
+            className="shrink-0 flex justify-start items-center"
+            style={{
+              transform:
+                currentPage == 0
+                  ? "translateX(0)"
+                  : currentPage + 2 >= Math.floor(productsCount / 10) - 1
+                  ? `translateX(-${
+                      (Math.floor(productsCount / 10) - 5) * 40
+                    }px)`
+                  : `translateX(-${(currentPage - 1) * 40}px)`,
+            }}
           >
-            {i + 1}
-          </PaginationLink>
-        ))}
+            {Array.from({ length: Math.floor(productsCount / 10) }).map(
+              (_, i, array) =>
+                i !== 0 &&
+                i !== array.length - 1 && (
+                  <PaginationLink
+                    key={i}
+                    isActive={i == currentPage}
+                    onClick={() => setCurrentPage(i)}
+                  >
+                    {i + 1}
+                  </PaginationLink>
+                )
+            )}
+          </div>
+        </div>
+
+        <PaginationLink
+          isActive={Math.floor(productsCount / 10) - 1 == currentPage}
+          onClick={() => setCurrentPage(Math.floor(productsCount / 10) - 1)}
+        >
+          {Math.ceil(productsCount / 10)}
+        </PaginationLink>
 
         <PaginationNext
           onClick={() =>
-            handleParams(
-              Number(page) < Math.ceil(productsCount / 10)
-                ? Number(page) + 1
-                : Math.ceil(productsCount / 10)
+            setCurrentPage(
+              currentPage < Math.floor(productsCount / 10)
+                ? currentPage + 1
+                : Math.floor(productsCount / 10)
             )
           }
         />
